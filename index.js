c4b8bc10db8d1518f07232a5bb2bddede7ed3ce7
zonafunapp-code <zonafunapp@gmail.com>
2025-09-23 13:25:46 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Asegúrate de que esta URL sea la de tu Google Apps Script
+const api_url = 'https://script.google.com/macros/s/AKfycbwsbH9SpKMBdXMkMwgxMl-zwWa2NvrtJq50BKiJpiSJiLv8r4i6d9vxKV2JvdSK1MFt/exec';
+
+// Este es el único endpoint que usaremos para nuestra API
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(api_url);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error al obtener los productos:', error.message);
+        res.status(500).send('Error al obtener los productos de la API');
+    }
+});
+
+module.exports = app;
